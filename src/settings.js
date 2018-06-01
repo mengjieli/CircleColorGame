c86178d7cc559ec685bf056076016b18dc121cd6
mengjieli <279037995@qq.com>
2018-06-01 16:25:59 +0800

    上传网站

---
+++ src/settings.js
@@ -0,0 +1,452 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "88MINJgFNAL4zBAaO0o9l9": [
+                "resources/circleGame/circleGame.prefab",
+                "cc.Prefab"
+            ],
+            "47NZqXZZhDqbKreUDaKGV2": [
+                "resources/circleGame/res/image/center",
+                "cc.SpriteFrame",
+                1
+            ],
+            "48RhcPv4ZF6KXoFAoLvFi9": [
+                "resources/circleGame/res/image/center.png",
+                "cc.Texture2D"
+            ],
+            "68TlhBxNVEkaFUMlWDeTC1": [
+                "resources/circleGame/res/image/color0_0",
+                "cc.SpriteFrame",
+                1
+            ],
+            "1dAbz6eqFLFKxukkuyextL": [
+                "resources/circleGame/res/image/color0_0.png",
+                "cc.Texture2D"
+            ],
+            "110s1T4exLVqf/ak8mZQlX": [
+                "resources/circleGame/res/image/color0_1",
+                "cc.SpriteFrame",
+                1
+            ],
+            "1fW9idTGRPOaHTvge+Bc4q": [
+                "resources/circleGame/res/image/color0_1.png",
+                "cc.Texture2D"
+            ],
+            "47ICca/P5JxIfQI+Weeoxn": [
+                "resources/circleGame/res/image/color0_2",
+                "cc.SpriteFrame",
+                1
+            ],
+            "727Chf+4pD6a4xjpmFG5Wl": [
+                "resources/circleGame/res/image/color0_2.png",
+                "cc.Texture2D"
+            ],
+            "4cAPQTWv9Lv7xEp5upMFD3": [
+                "resources/circleGame/res/image/color1_0",
+                "cc.SpriteFrame",
+                1
+            ],
+            e55QVPiTlHPZYg4Tb2eytG: [
+                "resources/circleGame/res/image/color1_0.png",
+                "cc.Texture2D"
+            ],
+            b1KNhCE5RAB6S5pEDj2rDk: [
+                "resources/circleGame/res/image/color1_1",
+                "cc.SpriteFrame",
+                1
+            ],
+            "0bfpVxEU1GR42VZ/iVzkuh": [
+                "resources/circleGame/res/image/color1_1.png",
+                "cc.Texture2D"
+            ],
+            "61/xkCISpCYIz9CW3t/7A8": [
+                "resources/circleGame/res/image/color1_2",
+                "cc.SpriteFrame",
+                1
+            ],
+            "8cuFny6btE7Kto6hp8Waqk": [
+                "resources/circleGame/res/image/color1_2.png",
+                "cc.Texture2D"
+            ],
+            "50msh8qJdOfpKMOs/ojkVG": [
+                "resources/circleGame/res/image/color2_0",
+                "cc.SpriteFrame",
+                1
+            ],
+            "44qP3OyApLJZq2xHXt/UQ8": [
+                "resources/circleGame/res/image/color2_0.png",
+                "cc.Texture2D"
+            ],
+            "3a9A07nI5F87YX79SidOq7": [
+                "resources/circleGame/res/image/color2_1",
+                "cc.SpriteFrame",
+                1
+            ],
+            "4dJoc5hptNha8qLVvqnTAH": [
+                "resources/circleGame/res/image/color2_1.png",
+                "cc.Texture2D"
+            ],
+            "7d6UbDYB9OyaQaGCHtIiXv": [
+                "resources/circleGame/res/image/color2_2",
+                "cc.SpriteFrame",
+                1
+            ],
+            "51UPWi3qVIULem+cnU3FVp": [
+                "resources/circleGame/res/image/color2_2.png",
+                "cc.Texture2D"
+            ],
+            "2c6Ul9JJ1FQrMDs2j9EI1d": [
+                "resources/circleGame/res/image/color3_0",
+                "cc.SpriteFrame",
+                1
+            ],
+            a6RIhJ67NIF6ddK06VPyAC: [
+                "resources/circleGame/res/image/color3_0.png",
+                "cc.Texture2D"
+            ],
+            "5d0a7L1yZB15icpOehEY9c": [
+                "resources/circleGame/res/image/color3_1",
+                "cc.SpriteFrame",
+                1
+            ],
+            bfFJRHDUdH04nyWOCiTBUY: [
+                "resources/circleGame/res/image/color3_1.png",
+                "cc.Texture2D"
+            ],
+            "16MF+luWdKb75w44hhTCL0": [
+                "resources/circleGame/res/image/color3_2",
+                "cc.SpriteFrame",
+                1
+            ],
+            "14qj2Ukc9IHoSBu9J9HXin": [
+                "resources/circleGame/res/image/color3_2.png",
+                "cc.Texture2D"
+            ],
+            "14fJRA1nlP8rZzJU/6Z7ic": [
+                "resources/circleGame/res/image/color4_0",
+                "cc.SpriteFrame",
+                1
+            ],
+            "0dpKmZM7NGzb2jVXD/m+pp": [
+                "resources/circleGame/res/image/color4_0.png",
+                "cc.Texture2D"
+            ],
+            e0XnfvhFFMVa7LUmXKcdjG: [
+                "resources/circleGame/res/image/color4_1",
+                "cc.SpriteFrame",
+                1
+            ],
+            "52qRGtY9hNxq/ZfyFzeJI1": [
+                "resources/circleGame/res/image/color4_1.png",
+                "cc.Texture2D"
+            ],
+            "61CE59hORD3677aCLEcYLL": [
+                "resources/circleGame/res/image/color4_2",
+                "cc.SpriteFrame",
+                1
+            ],
+            "76hFf/EkpBDJWBaIsiQMWk": [
+                "resources/circleGame/res/image/color4_2.png",
+                "cc.Texture2D"
+            ],
+            "0b/3x310lLIpTi4Dw7stMO": [
+                "resources/circleGame/res/image/color5_0",
+                "cc.SpriteFrame",
+                1
+            ],
+            d29DTV0bhKibfp5oIKD7cf: [
+                "resources/circleGame/res/image/color5_0.png",
+                "cc.Texture2D"
+            ],
+            "97KNXa261EmIJoDBebng+g": [
+                "resources/circleGame/res/image/color5_1",
+                "cc.SpriteFrame",
+                1
+            ],
+            "9dYvc5IWBDeKsGj0UTiK2g": [
+                "resources/circleGame/res/image/color5_1.png",
+                "cc.Texture2D"
+            ],
+            a5bGAglPxMLoNN0XFr7meh: [
+                "resources/circleGame/res/image/color5_2",
+                "cc.SpriteFrame",
+                1
+            ],
+            d7AXUdSAVL8pJcioM0tkrK: [
+                "resources/circleGame/res/image/color5_2.png",
+                "cc.Texture2D"
+            ],
+            "156Vsu54ZN7KxC/c5jrC1H": [
+                "resources/circleGame/res/image/line0",
+                "cc.SpriteFrame",
+                1
+            ],
+            "88Mq+ad5BBxa5fLgqQg8Ly": [
+                "resources/circleGame/res/image/line0.png",
+                "cc.Texture2D"
+            ],
+            d75nNsbnhFRZ7pFRi53Qrp: [
+                "resources/circleGame/res/image/line1",
+                "cc.SpriteFrame",
+                1
+            ],
+            "3aFq9C1QlHJriqXJLEmr/l": [
+                "resources/circleGame/res/image/line1.png",
+                "cc.Texture2D"
+            ],
+            "86oS81kjlANLV3aHDa1jWi": [
+                "resources/circleGame/res/image/line2",
+                "cc.SpriteFrame",
+                1
+            ],
+            f0PdsenlZGvZY5A16uhrZw: [
+                "resources/circleGame/res/image/line2.png",
+                "cc.Texture2D"
+            ],
+            dawLIuUdRNl7t3kD7i49Qo: [
+                "resources/circleGame/res/image/line3",
+                "cc.SpriteFrame",
+                1
+            ],
+            "09rjsfP+dC6L8VkOmD79QS": [
+                "resources/circleGame/res/image/line3.png",
+                "cc.Texture2D"
+            ],
+            "213tj+qiZMq7HTEOw/21kB": [
+                "resources/circleGame/res/image/line4",
+                "cc.SpriteFrame",
+                1
+            ],
+            "5bH/R3GwhG7L1TnaGiWVQ1": [
+                "resources/circleGame/res/image/line4.png",
+                "cc.Texture2D"
+            ],
+            f6HIlkaO1MKInh9fRWcPOd: [
+                "resources/circleGame/res/image/line5",
+                "cc.SpriteFrame",
+                1
+            ],
+            "68ji/6RSxDALEcsqDaq6Cw": [
+                "resources/circleGame/res/image/line5.png",
+                "cc.Texture2D"
+            ],
+            d9xlBl7O1BtK8bNahW3baL: [
+                "resources/circleGame/res/image/pao0",
+                "cc.SpriteFrame",
+                1
+            ],
+            "4afSOGSU5ECal6P4VxqVlV": [
+                "resources/circleGame/res/image/pao0.png",
+                "cc.Texture2D"
+            ],
+            "25Xv7uf81C5JpZ8VHRzJ4o": [
+                "resources/circleGame/res/image/pao1",
+                "cc.SpriteFrame",
+                1
+            ],
+            "88/OJUB29C9bSW55T468Ip": [
+                "resources/circleGame/res/image/pao1.png",
+                "cc.Texture2D"
+            ],
+            "77axlkwHpFm44ypz2XR0+i": [
+                "resources/circleGame/res/image/pao2",
+                "cc.SpriteFrame",
+                1
+            ],
+            "5dufowEFxLKKYmOHp4zs8U": [
+                "resources/circleGame/res/image/pao2.png",
+                "cc.Texture2D"
+            ],
+            "40hivICuNPKamy+4nePx2X": [
+                "resources/circleGame/res/image/pao3",
+                "cc.SpriteFrame",
+                1
+            ],
+            "34mAstb55OwJuftzjLlSPd": [
+                "resources/circleGame/res/image/pao3.png",
+                "cc.Texture2D"
+            ],
+            "1eFn4K9ShC5JsEEiOQ3c2D": [
+                "resources/circleGame/res/image/pao4",
+                "cc.SpriteFrame",
+                1
+            ],
+            "3e42CTDwRE15bTQOWwF0oI": [
+                "resources/circleGame/res/image/pao4.png",
+                "cc.Texture2D"
+            ],
+            "6eODQTBANEbaFyyGKaY4df": [
+                "resources/circleGame/res/image/pao5",
+                "cc.SpriteFrame",
+                1
+            ],
+            "2cB9M/qa9P450SmsdwTxUR": [
+                "resources/circleGame/res/image/pao5.png",
+                "cc.Texture2D"
+            ],
+            e1kMccYnhMNqWpLjVgdv8q: [
+                "resources/circleGame/res/image/环2/红色 中环",
+                "cc.SpriteFrame",
+                1
+            ],
+            "1a6i7siVJIX7OuBMeEqoiw": [
+                "resources/circleGame/res/image/环2/红色 中环.png",
+                "cc.Texture2D"
+            ],
+            "74amdOq2FG3JQPDMKYBjL2": [
+                "resources/circleGame/res/image/环2/红色 大环",
+                "cc.SpriteFrame",
+                1
+            ],
+            "2czgQ6h6BOFJT1c2d/tpR4": [
+                "resources/circleGame/res/image/环2/红色 大环.png",
+                "cc.Texture2D"
+            ],
+            "9cOSPdDcVIu4Cqcp1BO3Km": [
+                "resources/circleGame/res/image/环2/红色 小环",
+                "cc.SpriteFrame",
+                1
+            ],
+            cfFdKW5zlCNaC9YJUrVscz: [
+                "resources/circleGame/res/image/环2/红色 小环 - 副本",
+                "cc.SpriteFrame",
+                1
+            ],
+            "95+Teq0YlAkp6z5ida/Myh": [
+                "resources/circleGame/res/image/环2/红色 小环 - 副本.png",
+                "cc.Texture2D"
+            ],
+            "72fpXxyphHu7NoyKW2haIv": [
+                "resources/circleGame/res/image/环2/红色 小环.png",
+                "cc.Texture2D"
+            ],
+            "62pBGqeypFe4Y06CGCn0Lp": [
+                "resources/circleGame/res/image/环2/蓝色  中环",
+                "cc.SpriteFrame",
+                1
+            ],
+            c3YxZlkj5I1admkFFMsNOd: [
+                "resources/circleGame/res/image/环2/蓝色  中环.png",
+                "cc.Texture2D"
+            ],
+            "35csQbmnRBJp9rC9pVsdQy": [
+                "resources/circleGame/res/image/环2/蓝色  小环",
+                "cc.SpriteFrame",
+                1
+            ],
+            "f4IcSKRJNLlYeWGiMIk/xO": [
+                "resources/circleGame/res/image/环2/蓝色  小环 - 副本",
+                "cc.SpriteFrame",
+                1
+            ],
+            "888tAJsJ1JEJ21GHYxAOBx": [
+                "resources/circleGame/res/image/环2/蓝色  小环 - 副本.png",
+                "cc.Texture2D"
+            ],
+            "b2iCpxGptE+pUvE7GgPBEB": [
+                "resources/circleGame/res/image/环2/蓝色  小环.png",
+                "cc.Texture2D"
+            ],
+            "395ASQVOBNBpjFmn22rdEf": [
+                "resources/circleGame/res/image/环2/蓝色 大环",
+                "cc.SpriteFrame",
+                1
+            ],
+            "5fWwt1qY5KCrYMQ6QKLln5": [
+                "resources/circleGame/res/image/环2/蓝色 大环.png",
+                "cc.Texture2D"
+            ],
+            "79Y+E9bjRNe7rkXDxCReqz": [
+                "resources/circleGame/res/image/环2/黄色 中环",
+                "cc.SpriteFrame",
+                1
+            ],
+            "8agMtFf7FNtIVu2VurR8GF": [
+                "resources/circleGame/res/image/环2/黄色 中环.png",
+                "cc.Texture2D"
+            ],
+            "4dKDY1qNFCDoFkxyIZuU0t": [
+                "resources/circleGame/res/image/环2/黄色 大环",
+                "cc.SpriteFrame",
+                1
+            ],
+            "dc6M/1QqlEA7CjNFDAhELj": [
+                "resources/circleGame/res/image/环2/黄色 大环.png",
+                "cc.Texture2D"
+            ],
+            "13DOuptbZBjZ7sgdC3h3l3": [
+                "resources/circleGame/res/image/环2/黄色 小环",
+                "cc.SpriteFrame",
+                1
+            ],
+            "51zJTozctL8Jsf8luuCR8e": [
+                "resources/circleGame/res/image/环2/黄色 小环 - 副本",
+                "cc.SpriteFrame",
+                1
+            ],
+            "45WiQtT3JPEYnMbuqU7bkH": [
+                "resources/circleGame/res/image/环2/黄色 小环 - 副本.png",
+                "cc.Texture2D"
+            ],
+            "09WhTfAzhBNKbSgFsWTxsS": [
+                "resources/circleGame/res/image/环2/黄色 小环.png",
+                "cc.Texture2D"
+            ],
+            "80t6YSlyNAuqrb/b1cLik7": [
+                "resources/circleStart/circleStart.prefab",
+                "cc.Prefab"
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                "cc.Texture2D"
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                "cc.Texture2D"
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                "cc.Texture2D"
+            ],
+            d8HsitJHxOYqo801xBk8ev: [
+                "image/default_panel.png",
+                "cc.Texture2D"
+            ]
+        }
+    },
+    launchScene: "db://assets/Go.fire",
+    scenes: [
+        {
+            url: "db://assets/Go.fire",
+            uuid: "3cDYAIYy5E1rwgTvQ26sP6"
+        }
+    ],
+    packedAssets: {
+        "08d8bff97": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "80t6YSlyNAuqrb/b1cLik7",
+            "9bvaMerUlDyary99mJa6xp",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ],
+        "0e7c0f4a5": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "88MINJgFNAL4zBAaO0o9l9",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ]
+    },
+    orientation: "",
+    debug: true
+};
